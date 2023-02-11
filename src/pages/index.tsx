fd1025b3ea33c3f2316ca2c219f0d45b057f6e64
Ronni Hartlage <hartlage.7@wright.edu>
2023-02-10 22:51:16 -0500

    Updated backend and started adding tailwind to the frontend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,36 +4,44 @@ const Home = () => {
   const { data: session, status } = useSession();
 
   if (status === "loading") {
-    return <main>Loading...</main>;
+    return <main className="flex flex-col items-center pt-4">Loading...</main>;
   }
 
   return (
-    <main>
-      <h1>Guestbook</h1>
-      <div>
-        {session ? (
-          <>
-            <p>hi {session.user?.name}</p>
+    <main className="flex flex-col items-center">
+      <h1 className="text-3xl pt-4">Guestbook</h1>
+      <p>
+        Tutorial for <code>create-t3-app</code>
+      </p>
+      <div className="pt-10">
+        <div>
+          {session ? (
+            <>
+              <p className="mb-4 text-center">hi {session.user?.name}</p>
+              <button
+                type="button"
+                className="mx-auto block rounded-md bg-neutral-800 py-3 px-6 text-center hover:bg-neutral-700"
+                onClick={() => {
+                  signOut().catch(console.log);
+                }}
+              >
+                Logout
+              </button>
+            </>
+          ) : (
             <button
+              type="button"
+              className="mx-auto block rounded-md bg-neutral-800 py-3 px-6 text-center hover:bg-neutral-700"
               onClick={() => {
-                signOut().catch(console.log);
+                signIn("discord").catch(console.log);
               }}
             >
-              Logout
+              Login with Discord
             </button>
-          </>
-        ) : (
-          <button
-            onClick={() => {
-              signIn("discord").catch(console.log);
-            }}
-          >
-            Login with Discord
-          </button>
-        )}
+          )}
+        </div>
       </div>
     </main>
   );
 };
-
 export default Home;
