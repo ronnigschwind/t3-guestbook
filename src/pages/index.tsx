204ecdd4132879f11496f5e9dec6be80c2d6a242
Ronni Hartlage <hartlage.7@wright.edu>
2023-02-10 23:32:33 -0500

    When logged into GitHub, display option to add messages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useState } from "react";
 import { api } from "../utils/api";
 
 const GuestbookEntries = () => {
@@ -17,6 +18,60 @@ const GuestbookEntries = () => {
         );
       })}
     </div>
+  );
+};
+
+const Form = () => {
+  const [message, setMessage] = useState("");
+  const { data: session, status } = useSession();
+  
+  const utils = api.useContext();
+  const postMessage = api.guestbook.postMessage.useMutation({
+    onMutate: async (newEntry) => {
+      await utils.guestbook.getAll.cancel();
+      utils.guestbook.getAll.setData(undefined, (prevEntries) => {
+        if (prevEntries) {
+          return [newEntry, ...prevEntries];
+        } else {
+          return [newEntry];
+        }
+      });
+    },
+    onSettled: async () => {
+      await utils.guestbook.getAll.invalidate();
+    },
+  });
+  
+  if (status !== "authenticated") return null;
+
+  return (
+    <form
+      className="flex gap-2"
+      onSubmit={(event) => {
+        event.preventDefault();
+        postMessage.mutate({
+          name: session.user?.name as string,
+          message,
+        });
+        setMessage("");
+      }}
+    >
+      <input
+        type="text"
+        className="rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 focus:outline-none"
+        placeholder="Your message..."
+        minLength={2}
+        maxLength={100}
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}
+      />
+      <button
+        type="submit"
+        className="rounded-md border-2 border-zinc-800 p-2 focus:outline-none"
+      >
+        Submit
+      </button>
+    </form>
   );
 };
 
@@ -47,6 +102,9 @@ const Home = () => {
               >
                 Logout
               </button>
+              <div className="pt-6">
+                <Form />
+              </div>
             </>
           ) : (
             <button
